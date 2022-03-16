ad43fbc197a2002ad79c4ef93f7f2bdbef1c5ecd
lifehackerhansol <lifehacker@hansol.ca>
2022-03-16 10:11:10 -0700

    Switch to cx_Freeze

--- generator/core.py
+++ generator/core.py
@@ -1,3 +1,26 @@
+"""
+YANBF
+Copyright © 2022-present lifehackerhansol
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
 import subprocess
 import io
 import os
@@ -221,7 +244,7 @@ def downloadfromapi(path, boxart=None, sound=None):
     elif boxartdl:
         return 3
     return 0
-                    
+
 
 def downloadboxart(path, boxart=None):
     # get boxart for DS, to make banner
---
+++ generator/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+# "packages": ["os"] is used as example only
+build_exe_options = {"packages": ["libscrc", "PIL", "requests"], "excludes": ["tkinter, PyQt5"], 'build_exe': "dist"}
+
+# base="Win32GUI" should be used only for Windows GUI app
+base = None
+
+setup(
+    name="generator",
+    version="1.5.0",
+    description="YANBF Generator",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("generator.py", base=base)],
+)
